568c41e8dfc21441cd320fb352fa8d3cadb2de0c
Kunal <kunalmirgal99@gmail.com>
2025-07-05 15:32:41 +0530

    update

--- src/App.js
+++ src/App.js
@@ -12,44 +12,60 @@ import { useState } from 'react';
 
 
 function App() {
+  let initTodo; 
+  if (localStorage.getItem("todos")===null){
+     initTodo = []
+
+  }else{
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+    
+    
+     
 
   const onDelete =(todo)=> {
     console.log("i am onDelete of todo", todo);
 
     setTodos(todos.filter((e)=>{
       return e!==todo
-    }))
+    }));
+
+    localStorage.setItem("todos",JSON.stringify(todos)); 
   }
 
   const addTodo = (title,desc)=>{
     console.log("i am adding todo",title,desc);
-  }
-  const[todos,setTodos] = useState([
-    {
-      sno:1,
-      title:"go to the market",
-      desc: "you need to buy vegis"
-    },
-    {
-      sno:2,
-      title:"go to the gym",
-      desc: "you need to workout"
-    },
-    {
-   
-      sno:3,
-      title:"go to the office",
-      desc: "you need to work" 
+    let sno; 
+    if(todos.length === 0){
+      sno = 1;
     }
+    else{
+       sno = todos[todos.length -1].sno + 1;
+    }
+     
+    const myTodo ={
+      sno: sno,
+      title: title,
+      desc: desc
+    }
+    setTodos([...todos,myTodo]);
+    console.log(myTodo);
+
+
+  
+   localStorage.setItem("todos",JSON.stringify(todos));
+      
     
-  ]);
+    
+  }
+  const[todos,setTodos] = useState([initTodo]);
   
   return (
     <>
     <Header title ="Kunal Todos"  listOptions = {true}/>
     <AddTodos addTodos = {addTodo}/>
     <Todos todos={todos} onDelete={onDelete} />
-    {/* <Footer/>  */}
+    <Footer/> 
     </>
   );
 }
--- src/mycomponent/footer.js
+++ src/mycomponent/footer.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const footer = () => {
-  let footerStyle = {
-    position : "absolute",
-    width: "100%",
-    top: "100vh",
-  }
+  // let footerStyle = {
+  //   position : "relative",
+  //   width: "100%",
+  //   top: "70vh",
+  // }
   return ( 
-    <footer className="bg-dark text-light py-3"style={footerStyle}>
+    <footer className="bg-dark text-light py-3">
       <p className='text-center'>
         copyright &copy; 2023 KunalTodos.com All Rights Reserved.
       </p>
